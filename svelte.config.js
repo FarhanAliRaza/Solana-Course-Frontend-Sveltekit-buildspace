a027cbe66f46e2ce9d0b8b498c2a8d9880621a34
farhan <farhanalirazaazeemi@gmail.com>
2022-01-12 21:08:12 +0500

    again done

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,11 @@
-// import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-static';
 
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false
-		}),
+		adapter: adapter(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
